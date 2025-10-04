47ad601ed6cdde424af51ad543eccc2781a8e76e
Balsz <filecloudrom@gmail.com>
2025-10-05 01:33:06 +0700

    Evos

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -10,23 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Boot animaton
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-
-
 # Inherit some common device props
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := Tokodepia
-FULL_SYSTEM_OPTIMIZE_JAVA := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
-TARGET_BUILD_DEVICE_AS_WEBCAM := true
-TARGET_ENABLE_BLUR := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_EXCLUDES_AUDIOFX := true
+TARGET_DISABLE_EPPE := true
 TARGET_HAS_UDFPS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 WITH_GAPPS := false
