73ce97f68000ab5252d118a09fe509b289755931
Balsz <filecloudrom@gmail.com>
2025-10-06 08:19:14 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,11 +3,11 @@
 
 # Clone Kernel tree
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/BalsszZoldyck/android_kernel_asus_sdm660 -b wip kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/BalsszZoldyck/android_kernel_asus_sdm660 -b stable kernel/asus/sdm660
 
 # Clone Common tree
 rm -rf device/asus/sdm660-common
-git clone --depth=1 https://github.com/BalsszZoldyck/device_asus_sdm660-common -b los device/asus/sdm660-common
+git clone --depth=1 https://github.com/BalsszZoldyck/android_device_asus_sdm660-common-4.19 -b matrixx device/asus/sdm660-common
 
 # Clone Vendor tree
 rm -rf vendor/asus
