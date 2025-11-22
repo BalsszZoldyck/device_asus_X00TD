5e08b63063cbbca6cfde4efc67ce93db6b18d6a5
Balsz <filecloudrom@gmail.com>
2025-11-22 16:43:37 +0700

    Update and rename lineage_X00TD.mk to clover_X00TD.mk

--- clover_X00TD.mk
+++ clover_X00TD.mk
@@ -9,32 +9,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common crDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover stuff
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-
-
 # Inherit some common device props
-AXION_MAINTAINER := BllsszzXD
-AXION_PROCESSOR := Snapdragon_8s_Gen_Elite
-AXION_CAMERA_REAR_INFO := 13,5
-AXION_CAMERA_FRONT_INFO := 8
+CLOVER_MAINTAINER := BllsszzXD
+CLOVER_BUILDTYPE := UNOFFICIAL
 TARGET_DISABLE_EPPE := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := false
 FULL_SYSTEM_OPTIMIZE_JAVA := true
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
-TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_EXCLUDES_AUDIOFX := false
-TARGET_HAS_UDFPS := false
+WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := clover_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
