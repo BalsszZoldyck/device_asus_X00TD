c08addcb78d0e99fc03bf85e33960d46cfd8b7d7
Balsz <filecloudrom@gmail.com>
2025-11-25 01:14:51 +0700

    Update infinity_X00TD.mk

--- infinity_X00TD.mk
+++ infinity_X00TD.mk
@@ -15,10 +15,8 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-
-
 # Inherit some common device props
-INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_BUILD_TYPE := Spesial build
 INFINITY_MAINTAINER := BllsszzXD
 FULL_SYSTEM_OPTIMIZE_JAVA := true
 SYSTEM_OPTIMIZE_JAVA := true
@@ -29,7 +27,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_EXCLUDES_AUDIOFX := false
 TARGET_HAS_UDFPS := false
 TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GAPPS := false
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := infinity_X00TD
