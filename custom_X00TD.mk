4489e68f9dba49e4566b2ba45847fd046e3a66c8
BllsszzXD <filecloudrom@gmail.com>
2026-01-02 10:08:21 +0700

    X00TD : PixelOs

--- custom_X00TD.mk
+++ custom_X00TD.mk
@@ -10,27 +10,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common device props
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := BllsszzXD
-FULL_SYSTEM_OPTIMIZE_JAVA := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_DISABLE_EPPE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 TARGET_ENABLE_BLUR := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_EXCLUDES_AUDIOFX := false
-TARGET_HAS_UDFPS := false
-TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GAPPS := false
+WITH_GMS := true
+
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_X00TD
+PRODUCT_NAME := custom_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
