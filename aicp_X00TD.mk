cc2eabef49778ec0902f01d247d8970de4e7126f
BllsszzXD <filecloudrom@gmail.com>
2026-01-06 08:38:01 +0700

    X00TD : AICP

--- aicp_X00TD.mk
+++ aicp_X00TD.mk
@@ -10,27 +10,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# AICP STUF
+AICP_MAINTAINER := BllsszzXD
+DEVICE_MAINTAINER := BllsszzXD
+MAINTAINER_NAME := BllsszzXD
+#WITH_GMS := true
+
 # Inherit some common device props
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := BllsszzXD
 FULL_SYSTEM_OPTIMIZE_JAVA := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
-TARGET_ENABLE_BLUR := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_EXCLUDES_AUDIOFX := false
-TARGET_HAS_UDFPS := false
 TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GAPPS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_X00TD
+PRODUCT_NAME := aicp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
